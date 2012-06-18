45e853e664ba8be8f18a6c1d18ea629be3256eaf
Ezequiel Dario Gambaccini <ezequieldariogambaccini@Ezequiel-Dario-Gambaccinis-MacBook-Pro.local>
2012-06-18 14:39:59 -0300

    Objeto Gauss Seidel para resolver sistemas de ecuaciones lineales

---
+++ Python/gauss_seidel.py
@@ -0,0 +1,24 @@
+from numpy import matlib
+from numpy import matrix, triu, tril, diag
+
+
+class Gauss_Seidel(object):
+	"""Objeto que resuelve un sistema A,B,X dado con precision E for Gauss_Seidel"""
+	def __init__(self, A,B):
+		super(Gauss_Seidel, self).__init__()
+		D_L = tril(A)
+		U = triu(A,k=1)
+		self.Tgs = D_L.I * (-U)
+		self.Cgs = D_L.I * B
+
+	def solve(self, X):
+		return (self.Tgs * X + self.Cgs)
+	
+	def solve_precision(self,X,precision):
+		
+
+
+
+if __name__ == '__main__':
+	
+	A = matrix('1 2; 3 4')
